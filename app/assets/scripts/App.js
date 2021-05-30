fcc8ffe0343ee86cf53652f5bd994ad269d78f37
npxrus <npxrus@yandex.com>
2021-05-30 20:18:07 +0300

    Completed sticky header functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,11 +1,13 @@
 import "../styles/styles.css";
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
+import StickyHeader from "./modules/StickyHeader";
 
 const mobileMenu = new MobileMenu();
+const stickyHeader = new StickyHeader();
 
-new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
-new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 
 if (module.hot) {
   module.hot.accept();
