aca764f04a45769d234b10e407a0a34264b2dda8
npxrus <npxrus@yandex.com>
2021-05-31 19:32:12 +0300

    Completed the modal overlay

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,11 +3,29 @@ import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
 
-const mobileMenu = new MobileMenu();
-const stickyHeader = new StickyHeader();
+let modal;
 
+new MobileMenu();
+new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
+
+document.querySelectorAll(".open-modal").forEach((el) => {
+  el.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (typeof modal === "undefined") {
+      import("./modules/Modal")
+        .then((item) => {
+          modal = new item.default();
+          setTimeout(() => modal.openTheModal(), 20);
+        })
+        .catch((err) => console.error(err));
+    } else {
+      modal.openTheModal();
+    }
+  });
+});
 
 if (module.hot) {
   module.hot.accept();
